58dc626eacead32edd871dd8bcea6a1d09cc7d3b
Chiamaka Ugoji <mychiamaka@gmail.com>
2022-08-08 09:40:14 +0100

    Update image preview

--- services/api.ts
+++ services/api.ts
@@ -2,22 +2,23 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { AllBreedsResponse, RequestResponse } from "./dogType";
 
 interface ImagePayload {
-  breed: string;
-  subBreed: string;
+  breed:string;
+  subBreed?: string;
 }
+
 
 export const dogApi = createApi({
   reducerPath: "dogApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://dog.ceo/api/" }),
   endpoints: (builder) => ({
     getDogsByBreed: builder.mutation<RequestResponse, string>({
-      query: (breed: string) => `breed/${breed}/list`,
+      query: (breed) => `breed/${breed}/list`,
     }),
     getAllBreeds: builder.query<AllBreedsResponse, void>({
       query: () => "breeds/list/all",
     }),
     getBreedsImages: builder.mutation<RequestResponse, ImagePayload>({
-      query: ({ breed, subBreed }) => `breed/${breed}/${subBreed}/images`,
+      query: ({breed, subBreed})  => `breed/${breed}/${subBreed ? `${subBreed}/images`: "images"}`,
     }),
   }),
 });
